1b864dc942dcbc74fc4149f265078260bde5c298
Anney Park <anneypark@gmail.com>
2020-07-20 23:30:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,13 +6,17 @@ def loop_message_five_times(message)
   end
 end
 
-def loop_message_n_times(message)
-  count = n 
-  while count < do
-    puts message
+def loop_message_n_times(message, integer)
+  count = integer
+  while count  do
+    puts message 
     count
 end
 end 
 
-def output_array(message)
-  
+def output_array(message, integer)
+  count = integer
+  while 
+end
+
+def return_string_array(array)
